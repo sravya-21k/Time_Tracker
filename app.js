887002535916cdbac32c42415e1e2769c41b5513
sravya <sravya.kodehode@gmail.com>
2024-11-10 21:09:12 +0100

    ferdig oppgave

--- app.js
+++ app.js
@@ -37,27 +37,32 @@ document.addEventListener("DOMContentLoaded", () => {
       monthly: { current: 7, previous: 11 },
     },
   ];
-
+  //Get references to the timeFrame buttons
   const dailyBtn = document.getElementById("daily-btn");
   const weeklyBtn = document.getElementById("weekly-btn");
   const monthlyBtn = document.getElementById("monthly-btn");
 
+  //Function to update activity data based on the selected timeframe
   const updateTimeFrames = (timeFrame) => {
+    //Get all activity sections
     const activityElements = document.querySelectorAll(".section");
 
+    //Determine the label for the previous period based on the timeFrame
     const title =
       timeFrame === "daily"
-        ? "Yesterday"
+        ? "Yesterday" //If timeFrame is "daily" assign "Yesterday"
         : timeFrame === "weekly"
-        ? "Last Week"
-        : "Last Month";
+        ? "Last Week" //If timeFrame is "weekly" assign "Last week"
+        : "Last Month"; //If neither "daily" nor "weekly", assign "Last month"
+
+    //Loop through each activity element and update its content
     activityElements.forEach((element, index) => {
-      const { current, previous } = activities[index][timeFrame];
-      element.querySelector("h2").textContent = `${current}hrs`;
-      element.querySelector("p").textContent = `${title} - ${previous}hrs`;
+      const { current, previous } = activities[index][timeFrame]; //Get current and previous hours
+      element.querySelector("h2").textContent = `${current}hrs`; //update current hours
+      element.querySelector("p").textContent = `${title} - ${previous}hrs`; //update previous period
     });
   };
-  dailyBtn.addEventListener("click", () => updateTimeFrames("daily"));
-  weeklyBtn.addEventListener("click", () => updateTimeFrames("weekly"));
-  monthlyBtn.addEventListener("click", () => updateTimeFrames("monthly"));
+  dailyBtn.addEventListener("click", () => updateTimeFrames("daily")); //update data for daily timeFrame
+  weeklyBtn.addEventListener("click", () => updateTimeFrames("weekly")); //update data for weekly timeFrame
+  monthlyBtn.addEventListener("click", () => updateTimeFrames("monthly")); //Update data for monthly timeFrame
 });
